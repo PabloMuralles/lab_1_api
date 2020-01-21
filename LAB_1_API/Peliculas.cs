3726c5de5ea9076fafa2f538c903bee76de84371
PabloMuralles <pablomuralles28@gmail.com>
2020-01-21 12:39:17 -0600

    creacion de clase peliculas

---
+++ LAB_1_API/Peliculas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LAB_1_API
+{
+    public class Peliculas
+    {
+        public string Name { get; set; }
+
+        public int Year { get; set; }
+        public string Director { get; set; }
+    }
+}
