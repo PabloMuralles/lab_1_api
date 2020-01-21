805c955db917aba542617101173405eeca1b449f
Santiagobocel <santiagobocel10@gmail.com>
2020-01-21 13:12:06 -0600

    Get

---
+++ LAB_1_API/Controllers/peliculasController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+namespace LAB_1_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class peliculasController : ControllerBase
+    {
+        [HttpGet]
+        [Route("api/Pelicula")]
+        public ActionResult<string> Peliculas()
+        {
+            var peli = Estructuras.Almacenamiento.Instance.mostrar();
+
+            var json = JsonConvert.SerializeObject(peli);
+            return json;
+        }
+        [HttpPost]
+        public void llenado(string name, int date, string manager)
+        {
+            Estructuras.Almacenamiento.Instance.llenado_pila(name, date, manager);
+        }
+    }
+}
---
+++ LAB_1_API/Estructuras/Almacenamiento.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LAB_1_API.Estructuras
+{
+    public class Almacenamiento
+    {
+        private static Almacenamiento _instance = null;
+        public static Almacenamiento Instance
+        {
+            get
+            {
+                if (_instance == null) _instance = new Almacenamiento();
+                return _instance;
+            }
+        }
+
+        public Stack<Peliculas> listado = new Stack<Peliculas>();
+        public List<Peliculas> nuevalista = new List<Peliculas>();
+        public void llenado_pila(string N, int D, string M)
+        {
+            listado.Push(new Peliculas
+            {
+                Name = N,
+                Year = D,
+                Director = M
+            });
+        }
+        public List<Peliculas> mostrar()
+        {
+            if (listado.Count > 10)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    nuevalista.Add(listado.Pop());
+                }
+            }
+            else
+            {
+                nuevalista = null;
+            }
+            return nuevalista;
+        }
+    }
+}
